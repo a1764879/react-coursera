90f87edf9954165bac3b29e055eae9906d8a7204
Alan Pallath <iamakidilam@gmail.com>
2020-06-24 18:08:53 +0930

    dishdetail component added

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
-
+import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import DishDetail from './DishdetailComponent';
 class Menu extends Component {
 
     constructor(props){
@@ -16,21 +16,9 @@ class Menu extends Component {
     }
 
     renderDish(dish) {
-        if(dish != null) {
-            return(
-                <Card>
-                    <CardImg width="100%"  src={dish.image} alt={dish.name}/>
-                    <CardBody>
-                    <CardTitle>{dish.name}</CardTitle>
-                    <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            )
-        } else {
-            return(
-                <div></div>
-            )
-        }
+        return(
+            <DishDetail selDish={dish} />
+        )
     }
 
 
