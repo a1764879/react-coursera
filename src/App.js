e3d6e7fa2a78f94adb199540a4dabf6496b0e479
Alan Pallath <iamakidilam@gmail.com>
2020-07-07 13:43:36 +0930

    Into to redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import Main from './components/MainComponent';
 import './App.css';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { ConfigureStore } from './redux/configureStore';
+
+const store = ConfigureStore();
 
 function App() {
   return (
-    <div className="App">
-      <Main />
-    </div>
+    <Provider store={store}>
+      <BrowserRouter>
+        <div className="App">
+          <Main />
+        </div>
+      </BrowserRouter>
+    </Provider>
+    
   );
 }
 
